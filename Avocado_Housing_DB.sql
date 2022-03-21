8be06a41bfc7199982d4497468d0a9a949b40792
Zach Hansen <zthansen86@gmail.com>
2022-03-21 12:07:27 -0400

    initial commit

---
+++ Avocado_Housing_DB.sql
@@ -0,0 +1,27 @@
+-- Create a new table
+CREATE TABLE Avocado_Housing_DB (
+  Date DATE NOT NULL,
+  SEA_AveragePrice REAL,
+  SEA_TotalVolume REAL,
+  Seattle REAL,
+  NSH_AveragePrice REAL,
+  NSH_TotalVolume REAL,
+  Nashville REAL,
+  NY_AveragePrice REAL,
+  NY_TotalVolume REAL,
+  NewYork REAL,
+  PTL_AveragePrice REAL,
+  PTL_TotalVolume REAL,
+  Portland REAL,
+  LA_AveragePrice REAL,
+  LA_TotalVolume REAL,
+  LosAngeles REAL,
+  ATL_AveragePrice REAL,
+  ATL_TotalVolume REAL,
+  Atlanta REAL,
+  CHI_AveragePrice REAL,
+  CHI_TotalVolume REAL,
+  Chicago REAL
+);
+
+SELECT * FROM Avocado_Housing_DB;
